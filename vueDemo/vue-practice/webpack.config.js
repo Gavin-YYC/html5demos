ecedef8d8d61ba045307c95cf8755e0ca369b3bd
Gavin <q414625852@163.com>
2016-11-19 12:17:50 +0800

    test html-webpack-plugin

--- vueDemo/vue-practice/webpack.config.js
+++ vueDemo/vue-practice/webpack.config.js
@@ -1,7 +1,10 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
-  entry: './main.js',
+  entry: './src/main.js',
   output: {
-    path: __dirname,
+    path: path.resolve(__dirname, './dist'),
     filename: 'build.js'
   },
   module: {
@@ -19,6 +22,13 @@ module.exports = {
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
+  plugins: [
+    // 自动生成index.html，添加script标签、hash值
+    new HtmlWebpackPlugin({
+      title: 'Gavin todolist',
+      hash: true
+    })
+  ],
   babel: {
     presets: ['es2015']
   }
