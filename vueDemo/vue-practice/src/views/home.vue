6f0621a62ea6d6a56eb4300cf4ce2d373f166ce8
Gavin <q414625852@163.com>
2016-11-22 11:51:05 +0800

    f

--- vueDemo/vue-practice/src/views/home.vue
+++ vueDemo/vue-practice/src/views/home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <p>{{home}}</p>
     <pre>{{JSON.stringify(admin, null, 4)}}</pre>
+    <el-alert title="成功提示的文案" type="success"></el-alert>
   </div>
 </template>
 
@@ -9,12 +10,17 @@
 
 import Auth from '../lib/auth'
 
+import {Alert} from 'element-ui'
+
 export default {
   data() {
     return {
       home: 'home',
       admin: {}
     }
+  },
+  components: {
+    "el-alert": Alert
   },
   // 组件编译挂载后执行
   mounted() {
--- vueDemo/vue-practice/webpack.config.js
+++ vueDemo/vue-practice/webpack.config.js
@@ -1,6 +1,7 @@
 var path = require('path')
 var webpack = require('webpack')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
+var ExtractTextPlugin = require("extract-text-webpack-plugin")
 
 module.exports = {
   entry: './src/main.js',
@@ -17,23 +18,34 @@ module.exports = {
       }, {
         test: /\.vue$/,
         loader: 'vue'
-      }
+      }, {
+        test: /\.css$/,
+        loaders: ['style', 'css']
+      }, {
+			  test:/\.css$/,
+  			loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+  		}, {
+  			test: /\.(woff2?|eot|ttf)(\?.*)?$/,
+  			loader: 'url',
+  			query: {
+  				limit: 10000,
+  				name: 'fonts/[name].[ext]'
+  			}
+  		}
     ]
   },
+
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
+  
   plugins: [
+    new ExtractTextPlugin('[name].css'),
     // 自动生成index.html，添加script标签、hash值
     new HtmlWebpackPlugin({
       title: 'Gavin todolist',
       hash: true,
       template: './src/index.html'
-    }),
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: 'production'
-      }
     }),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
@@ -41,7 +53,10 @@ module.exports = {
       }
     })
   ],
+
   babel: {
     presets: ['es2015']
-  }
+  },
+
+  devtool: false
 }
